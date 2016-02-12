18259989d26460c0bf4f325e85f43f41ed64806f
Kevin <1kevinxu1@gmail.com>
2016-02-12 18:28:01 -0500

    generate session controller and work on application controller helper methods for user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,30 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  helper_method :current_user, :require_user
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by_session_digest(session[:token])
+  end
+
+  def sign_in!(user)
+    @current_user = user
+    session[:token] = user.session_digest
+  end
+
+  def logged_in?
+    redirect_to root_url if current_user
+  end
+
+  def sign_out!
+    current_user.reset_session_digest
+    session[:token] = nil
+  end
+
+  def require_user
+    redirect_to new_session_url unless current_user
+  end
 end
